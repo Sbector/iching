4cf3e134d7270e434b7a7f8dfa49e3cdf649d9eb
No_otra_vez <saulbector@gmail.com>
2023-11-10 01:11:32 -0600

    Reduce number of buttons

--- app/components/CoinToss.jsx
+++ app/components/CoinToss.jsx
@@ -7,19 +7,29 @@ export default function CoinToss() {
   
     const tossCoins = () => {
       const newResults = []
-      for (let i = 0; i < 3; i++) {
-        // Simulate coin toss (2 = tails, 3 = heads)
-        const result = Math.floor(Math.random() * 2) + 2
-        newResults.push(result)
+      for(let toss = 0; toss < 6; toss++){
+        const tossResult = []
+        for (let i = 0; i < 3; i++) {
+          // Simulate coin toss (2 = tails, 3 = heads)
+          const result = Math.floor(Math.random() * 2) + 2
+          tossResult.push(result)
+        }
+      newResults.push(tossResult)
       }
       console.log(newResults)
       setCoinResults(newResults)
-    };
+    }
+      
   
     return (
       <div>
-        <Button onClick={tossCoins}>Toss Coins</Button>
-        <p>{coinResults}</p>
+        <a onClick={tossCoins}>
+        <Button>Toss Coins</Button>
+        </a>
+        {coinResults.map((tossResult, index) => (
+          <p key={index}>Toss {index + 1}: {tossResult.join(', ')}</p>
+        ))}
+        
       </div>
     )
 }
--- app/consult/page.jsx
+++ app/consult/page.jsx
@@ -4,15 +4,11 @@ import Question from "../components/Question";
 export default function Consult() {
     return(
         <div>
-            <h1>Let make a consult!</h1>
+            <h1 className="text-xl mb-6">Let make a consult!</h1>
             <Question/>
             <div>
                 <CoinToss/>
-                <CoinToss/>
-                <CoinToss/>
-                <CoinToss/>
-                <CoinToss/>
-                <CoinToss/>
+                
             </div>
         </div>
     )
