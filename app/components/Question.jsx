a3179f7bab15a2598675974c9ccc8c20cf7229cb
No_otra_vez <saulbector@gmail.com>
2023-11-10 17:01:58 -0600

    Style textarea

--- app/components/Question.jsx
+++ app/components/Question.jsx
@@ -2,7 +2,7 @@ export default function question() {
     return (
         <div>
             <label htmlFor="question">Enter your question </label><br/>
-            <textarea type="text" name="question" className="w-3/4 text-white bg-gray-600"/><br/><br/>
+            <textarea id="question" type="text" name="question" className="w-3/4 bg-gray-200 dark:bg-gray-300 dark:text-black"/>
         </div>
     )
 }
