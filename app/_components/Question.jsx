f453b1a214720c0f5a94a6a6ea7cc07f12c307de
Sbector <saulbector@gmail.com>
2024-11-23 23:27:19 -0600

    Change components folder visibility

--- app/_components/Question.jsx
+++ app/_components/Question.jsx
@@ -1,7 +1,7 @@
 export default function question() {
     return (
         <div>
-            <label htmlFor="question" className="text-left">Enter your question </label><br/>
+            <label htmlFor="question" className="">Enter your question </label><br/>
             <textarea id="question" type="text" name="question" className="min-w-full bg-gray-200 dark:bg-gray-300 dark:text-black"/>
         </div>
     )
